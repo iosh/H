4f816f33289a8d6b85da01f82678ad2402589a2d
HiMrHu <454053152@qq.com>
2020-04-27 16:40:57 +0800

    change travis

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ let cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
 
 // 压缩html
 gulp.task('minify-html', function() {
@@ -31,18 +30,7 @@ gulp.task('minify-js', function() {
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
-// 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src('./public/images/**/*.*')
-        .pipe(imagemin(
-        [imagemin.gifsicle({'optimizationLevel': 3}),
-        imagemin.jpegtran({'progressive': true}),
-        imagemin.optipng({'optimizationLevel': 7}),
-        imagemin.svgo()],
-        {'verbose': true}))
-        .pipe(gulp.dest('./public/images'))
-});
 // 默认任务
 gulp.task('default', gulp.parallel(
-    'minify-html','minify-css','minify-js','minify-images'
+    'minify-html','minify-css','minify-js'
 ));
